cc588d7bbd48505c8bb111449d523d0a34730b0d
Yichen Zhu <ethanmaiche@gmail.com>
2017-11-09 19:30:06 +0000

    final updates

--- YelpCamp/Final/app.js
+++ YelpCamp/Final/app.js
@@ -18,7 +18,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v9");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_final";
+//"mongodb://yichenzhu:yichenzhu@ds155315.mlab.com:55315/yelpcamp"
+mongoose.connect("mongodb://yichenzhu:yichenzhu@ds155315.mlab.com:55315/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
